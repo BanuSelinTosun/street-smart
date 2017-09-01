3ca5b21cd6d274d350ed6d09f652feebcfa9c6fd
BanuSelinTosun <selin.tosun@gmail.com>
2017-09-01 14:39:09 -0700

    Updated Regressor Modelling

--- src/SalePrice_Modelling.py
+++ src/SalePrice_Modelling.py
@@ -5,20 +5,37 @@ import numpy as np
 
 def feature_importance_cal(Model):
     importances = Model.feature_importances_
-    std = np.std([FSM.feature_importances_ for tree in FSM.estimators_], axis=0)
+    std = np.std([tree.feature_importances_ for tree in FSM.estimators_], axis=0)
     indices = np.argsort(importances)[::-1]
-    pass
+    return importances[indices]
 
 def modelling(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
-    Model = RandomForestRegressor()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
+    Model = RandomForestRegressor(n_estimators=300, n_jobs=-1, random_state=0)
     Model.fit(X_train, y_train)
     y_predict = Model.predict(X_test).astype(int)
     score = Model.score(X_test, y_test)
     y_true = y_test.as_matrix()
     residuals = np.log(y_true) - np.log(y_predict)
-    error = sum(abs(residuals))/len(y_predict)
+    error = abs(1 - np.exp(sum(abs(residuals))/len(y_predict)))*100
     return score, Model, y_predict, residuals, error
+
+def HeatSystem_converter(df):
+    HeatSystem_dummies = pd.get_dummies(df.HeatSystem, drop_first=False, prefix='HeatSystem')
+    df = pd.concat([df, HeatSystem_dummies], axis=1).drop('HeatSystem', axis=1)
+    return df
+
+def HeatSource_converter(df):
+    HeatSource_dummies = pd.get_dummies(df.HeatSource, drop_first=False, prefix='HeatSource')
+    df = pd.concat([df, HeatSource_dummies], axis=1).drop('HeatSource', axis=1)
+    return df
+
+def SaleWarning_conv(df):
+    df.SaleWarning = df.SaleWarning.apply(lambda x: x.split())
+    warningdummies = pd.get_dummies(df.SaleWarning.apply(pd.Series).stack(), prefix='SWarn', drop_first=False).sum(level=0)
+    df = df.join(warningdummies, how='left').fillna(0.0)
+    df = df.drop('SaleWarning', axis=1)
+    return df
 
 def Zipcode_converter(df):
     Zipcode_dummies = pd.get_dummies(df.Zipcode, drop_first=False)
@@ -28,16 +45,21 @@ def Zipcode_converter(df):
 def load_data():
     df = pd.read_pickle('ResAss_w_PbSch_Rtngs_Clnd_df.p')
     df['Zipcode'] = df['ZipCode_reduced'].astype(int)
-    df['Documentation_Age'] = 2017 - df['DocumentDate'].dt.year
+    df['Documentation_YearAge'] = 2017 - df['DocumentDate'].dt.year
+    df['Documentation_month'] = df['DocumentDate'].dt.month
+    df = df[df.Documentation_YearAge <= 1]
     df['TotalCost'] = df['SalePrice'] + df['AddnlCost']
-    uplimit = df.TotalCost.mean() + df.TotalCost.std()*4
-    bottomlimit = min(abs(df.TotalCost.mean() - df.TotalCost.std()*4), df.TotalCost.min())
-    df = df[(df.TotalCost > df.TotalCost.min()) & (df.TotalCost < (df.TotalCost.mean() + df.TotalCost.std()*4))]
+    uplimit = min(df.TotalCost.mean() + df.TotalCost.std()*3.5, 2000000)
+    bottomlimit = 100000 # min(abs(df.TotalCost.mean() - df.TotalCost.std()*4), df.TotalCost.min())
+    df = df[(df.TotalCost > bottomlimit) & (df.TotalCost < uplimit)]
     df = df.drop(['Address', 'StreetName', 'StreetType', 'SellerName', 'BuyerName', 'DirectionSuffix', 
-         'SaleWarning', 'parcel_number', 'PROP_NAME', 'ES_ZONE', 'MS_ZONE', 'HS_ZONE', 'LEVY_JURIS', 'SalePrice', 
-         'AddnlCost', 'ExciseTaxNbr', 'BldgGrade', 'SaleReason', 'SaleInstrument', 'FinBasementGrade', 'ZipCode_reduced',
-         'DocumentDate', 'SalePrice', 'AddnlCost'], axis=1, inplace=True)
+         'parcel_number', 'PROP_NAME', 'ES_ZONE', 'MS_ZONE', 'HS_ZONE', 'LEVY_JURIS', 'SalePrice', 
+         'AddnlCost', 'ExciseTaxNbr', 'BldgGrade', 'SaleReason', 'SaleInstrument', 'FinBasementGrade', 
+         'ZipCode_reduced', 'DocumentDate'], axis=1, inplace=True)
     df = Zipcode_converter(df)
+    df = SaleWarning_conv(df)
+    df = HeatSource_converter(df)
+    df = HeatSystem_converter(df)
     y = df.TotalCost
     X = df.drop('TotalCost', axis=1)
     del(df)
