725866a9c180e033293f9f4e3fbfdf119a2e0d39
BanuSelinTosun <selin.tosun@gmail.com>
2017-08-31 23:50:19 -0700

    Modelling Adjustments

--- src/SalePrice_Modelling.py
+++ src/SalePrice_Modelling.py
@@ -5,8 +5,7 @@ import numpy as np
 
 def feature_importance_cal(Model):
     importances = Model.feature_importances_
-    std = np.std([FSM.feature_importances_ for tree in FSM.estimators_],
-             axis=0)
+    std = np.std([FSM.feature_importances_ for tree in FSM.estimators_], axis=0)
     indices = np.argsort(importances)[::-1]
     pass
 
@@ -16,25 +15,38 @@ def modelling(X, y):
     Model.fit(X_train, y_train)
     y_predict = Model.predict(X_test).astype(int)
     score = Model.score(X_test, y_test)
-    return score, Model, y_predict
+    y_true = y_test.as_matrix()
+    residuals = np.log(y_true) - np.log(y_predict)
+    error = sum(abs(residuals))/len(y_predict)
+    return score, Model, y_predict, residuals, error
+
+def Zipcode_converter(df):
+    Zipcode_dummies = pd.get_dummies(df.Zipcode, drop_first=False)
+    df = pd.concat([df, Zipcode_dummies], axis=1).drop('Zipcode', axis=1)
+    return df
 
 def load_data():
     df = pd.read_pickle('ResAss_w_PbSch_Rtngs_Clnd_df.p')
     df['Zipcode'] = df['ZipCode_reduced'].astype(int)
-    subset_df['DocumentationYear'] = subset_df['DocumentDate'].dt.year
-    subset_df['TotalCost'] = subset_df['SalePrice'] + subset_df['AddnlCost']
-    subset_df = df.drop(['Address', 'StreetName', 'StreetType', 'SellerName', 'BuyerName', 'DirectionSuffix',
-                     'SaleWarning', 'ZipCode_reduced', 'parcel_number', 'PROP_NAME', 'ES_ZONE', 'MS_ZONE',
-                     'HS_ZONE', 'LEVY_JURIS', 'DocumentDate', 'SalePrice', 'AddnlCost', 'ExciseTaxNbr',
-                     'BldgGrade', 'SaleReason', 'SaleInstrument', 'FinBasementGrade'], axis=1)
-    y = subset_df.TotalCost
-    X = subset_df.drop('TotalCost', axis=1)
+    df['Documentation_Age'] = 2017 - df['DocumentDate'].dt.year
+    df['TotalCost'] = df['SalePrice'] + df['AddnlCost']
+    uplimit = df.TotalCost.mean() + df.TotalCost.std()*4
+    bottomlimit = min(abs(df.TotalCost.mean() - df.TotalCost.std()*4), df.TotalCost.min())
+    df = df[(df.TotalCost > df.TotalCost.min()) & (df.TotalCost < (df.TotalCost.mean() + df.TotalCost.std()*4))]
+    df = df.drop(['Address', 'StreetName', 'StreetType', 'SellerName', 'BuyerName', 'DirectionSuffix', 
+         'SaleWarning', 'parcel_number', 'PROP_NAME', 'ES_ZONE', 'MS_ZONE', 'HS_ZONE', 'LEVY_JURIS', 'SalePrice', 
+         'AddnlCost', 'ExciseTaxNbr', 'BldgGrade', 'SaleReason', 'SaleInstrument', 'FinBasementGrade', 'ZipCode_reduced',
+         'DocumentDate', 'SalePrice', 'AddnlCost'], axis=1, inplace=True)
+    df = Zipcode_converter(df)
+    y = df.TotalCost
+    X = df.drop('TotalCost', axis=1)
+    del(df)
     return X, y
 
 def main():
     X, y = load_data()
-    score, _, predicted_cost = modelling(X, y)
-    return score, predicted_cost
+    score, Model, predicted_cost, residuals, error = modelling(X, y)
+    return 'Model Fit Score:', score, 'Model:', Model, 'predicted_cost:', predicted_cost, 'Error:', error
 
 if __name__=="__main__":
     main()
