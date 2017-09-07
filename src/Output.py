9d46340495d102724b152d38847f4da331408c80
BanuSelinTosun <selin.tosun@gmail.com>
2017-09-07 15:42:56 -0700

    Clean Output

--- src/Output.py
+++ src/Output.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
+from collections import OrderedDict
 
 def Private_Schooling(age):
     """This function calculates the total Private School Cost per kid until College
         input: age of the kid, integer
         output: total cost until collage
-        Note: This calculation excludes bussing which is ~1100 $/year as of September 2017 in the city of Seattle""" 
+        Note: This calculation excludes bussing which is ~1100 $/year as of September 2017 in the city of Seattle"""
     Ave_Ann_Tuition_increase = 0.06
-    Prv_ES_MS_Tuiton = 13801 
-    Prv_HS_Tuiton = 14473 
+    Prv_ES_MS_Tuiton = 13801
+    Prv_HS_Tuiton = 14473
 
     if age<14:
         Prv_ES_MS_Cost = [Prv_ES_MS_Tuiton]
@@ -32,7 +33,7 @@ def Private_Schooling(age):
             Cost_HS = Prv_HS_Cost[i]*(1+Ave_Ann_Tuition_increase)
             Prv_HS_Cost.append(Cost_HS)
         Total_Prv_ES_MS_Cost = 0
-        Total_Prv_HS_Cost = sum(Prv_HS_Cost[0:18-age])   
+        Total_Prv_HS_Cost = sum(Prv_HS_Cost[0:18-age])
 
     Total = Total_Prv_ES_MS_Cost + Total_Prv_HS_Cost
     return Total
@@ -44,59 +45,43 @@ def total_kids_edu(age_lst):
     for age in age_lst:
         total_cost_per_kid = Private_Schooling(age)
         total.append(total_cost_per_kid)
-    return sum(total)    
+    return sum(total)
 
-def subsetting(Matrix, age_lst, SqFtLiving=700, Bedrooms=1):
-    Matrix_subset = Matrix[(Matrix.SqFtTotLiving > (SqFtLiving*0.90)) & (Matrix.SqFtTotLiving < (SqFtLiving*1.1)) & (Matrix.Bedrooms <= (Bedrooms+1)) & (Matrix.Bedrooms >= (Bedrooms-1))]
-    Matrix_98102 = Matrix_subset[Matrix_subset[98102]==1]
-    Matrix_98103 = Matrix_subset[Matrix_subset[98103]==1]
-    Matrix_98104 = Matrix_subset[Matrix_subset[98104]==1]
-    Matrix_98105 = Matrix_subset[Matrix_subset[98105]==1]
-    Matrix_98106 = Matrix_subset[Matrix_subset[98106]==1]
-    Matrix_98107 = Matrix_subset[Matrix_subset[98107]==1]
-    Matrix_98108 = Matrix_subset[Matrix_subset[98108]==1]
-    Matrix_98109 = Matrix_subset[Matrix_subset[98109]==1]
-    Matrix_98112 = Matrix_subset[Matrix_subset[98112]==1]
-    Matrix_98115 = Matrix_subset[Matrix_subset[98115]==1]
-    Matrix_98116 = Matrix_subset[Matrix_subset[98116]==1]
-    Matrix_98117 = Matrix_subset[Matrix_subset[98117]==1]
-    Matrix_98118 = Matrix_subset[Matrix_subset[98118]==1]
-    Matrix_98119 = Matrix_subset[Matrix_subset[98119]==1]
-    Matrix_98122 = Matrix_subset[Matrix_subset[98122]==1]
-    Matrix_98125 = Matrix_subset[Matrix_subset[98125]==1]
-    Matrix_98126 = Matrix_subset[Matrix_subset[98126]==1]
-    Matrix_98133 = Matrix_subset[Matrix_subset[98133]==1]
-    Matrix_98136 = Matrix_subset[Matrix_subset[98136]==1]
-    Matrix_98144 = Matrix_subset[Matrix_subset[98144]==1]
-    Matrix_98146 = Matrix_subset[Matrix_subset[98146]==1]
-    Matrix_98177 = Matrix_subset[Matrix_subset[98177]==1]
-    Matrix_98178 = Matrix_subset[Matrix_subset[98178]==1]
-    Matrix_98199 = Matrix_subset[Matrix_subset[98199]==1]
-    zipcode_lst = [Matrix_98102, Matrix_98103, Matrix_98104, Matrix_98105, Matrix_98106, Matrix_98107, Matrix_98108, Matrix_98109, Matrix_98112, Matrix_98115, Matrix_98116, Matrix_98117,
-               Matrix_98118, Matrix_98119, Matrix_98122, Matrix_98125, Matrix_98126, Matrix_98133, Matrix_98136, Matrix_98144, Matrix_98146, Matrix_98177, Matrix_98178, Matrix_98199]
-    Seattle_Zipcodes = [98102, 98103, 98104, 98105, 98106, 98107, 98108, 98109, 98112, 98115, 98116, 98117, 98118, 98119, 98122, 98125, 98126, 98133, 98136, 
-                    98144, 98146, 98177, 98178, 98199]
+def subsetting(Matrix, SqFtLiving=700, Bedrooms=1):
+    Matrix_subset = Matrix[(Matrix.SqFtTotLiving > (SqFtLiving*0.90)) &
+                           (Matrix.SqFtTotLiving < (SqFtLiving*1.1)) &
+                           (Matrix.Bedrooms <= (Bedrooms+1)) &
+                           (Matrix.Bedrooms >= (Bedrooms-1))]
+    Seattle_Zipcodes = [98102, 98103, 98104, 98105, 98106, 98107, 98108, 98109,
+                        98112, 98115, 98116, 98117, 98118, 98119, 98122, 98125,
+                        98126, 98133, 98136, 98144, 98146, 98177, 98178, 98199]
+    Zipcode_Matrix = OrderedDict()
+    for zipcode in Seattle_Zipcodes:
+        Zipcode_Matrix[zipcode]=Matrix_subset[Matrix_subset[zipcode]==1]
+    return Zipcode_Matrix
+
+def output_app(Matrix, age_lst, SqFtLiving, Bedrooms):
+    Zipcode_Matrix = subsetting(Matrix, SqFtLiving, Bedrooms)
     total_edu_cost = total_kids_edu(age_lst)
     print "{:5s}  {:4s} {:10s}   {:10s}  {:10s}  {:3s} {:3s} {:3s} {:10s}".format('Zip', 'Num', 'Min Cost', 'Ave Cost','Max Cost', 'ES', 'MS', 'HS', 'PrEdu Cst')
-    for code, matrix in zip(Seattle_Zipcodes, zipcode_lst):
+    for code, matrix in Zipcode_Matrix.items():
         if len(matrix)!=0:
-            print "{:5d} {:4d} {:10.2f} < {:10.2f} < {:10.2f} {:3.0f} {:3.0f} {:3.0f} {:10.2f}".format(code, len(matrix), 
-                                                                                 matrix.TotalCost.mean() - matrix.TotalCost.std()*1.96, matrix.TotalCost.mean(), 
-                                                                                 matrix.TotalCost.mean() + matrix.TotalCost.std()*1.96, matrix.ES_Ranking.median(), 
+            print "{:5d} {:4d} {:10.2f} < {:10.2f} < {:10.2f} {:3.0f} {:3.0f} {:3.0f} {:10.2f}".format(code, len(matrix),
+                                                                                 matrix.TotalCost.mean() - matrix.TotalCost.std()*1.96, matrix.TotalCost.mean(),
+                                                                                 matrix.TotalCost.mean() + matrix.TotalCost.std()*1.96, matrix.ES_Ranking.median(),
                                                                                  matrix.MS_Ranking.median(), matrix.HS_Ranking.median(), total_edu_cost)
 
 
 def load_data(SqFtLiving, Bedrooms, age_lst):
     Matrix = pd.read_pickle('Predicted_Matrix.p')
-    Output = subsetting(Matrix, age_lst, SqFtLiving, Bedrooms)
-    return Output
+    output_app(Matrix, age_lst, SqFtLiving, Bedrooms)
 
-def main(SqFtLiving, Bedrooms, age_lst):
-    Output = load_data(SqFtLiving, Bedrooms, age_lst)
-    
-if __name__=="__main__":
+def main():
     SqFtLiving = raw_input('Please enter the average SqFT you are looking for:')
     Bedrooms = raw_input('Please enter the number of Bedrooms you are looking for:')
     ages = raw_input('Please enter the age of each kid in the household, (in format e.g. 1 2 3):')
     age_lst = [int(x) for x in ages.split()]
-    main(float(SqFtLiving), float(Bedrooms), age_lst)
+    load_data(float(SqFtLiving), float(Bedrooms), age_lst)
+
+if __name__=="__main__":
+    main()
